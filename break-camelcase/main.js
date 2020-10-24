4cfa8fcc1d0060ce5acdab1971be480bd4cfb633
Paxnamra <bialyfeniks4@gmail.com>
2020-10-24 19:39:57 +0200

    code wars: break camelCase - refactored solution

--- break-camelcase/main.js
+++ break-camelcase/main.js
@@ -5,24 +5,7 @@ Test.assertEquals(solution('camelCasingTest'), 'camel Casing Test', 'Unexpected 
 
 // complete the function
 function solution(string) {
-    let camel = arrayString(string);
-
-    function arrayString(a) {
-        let camelCased = [];
-        for (let i = 0; i < a.length; i++) {
-            let ch = a.charAt(i);
-
-            if (ch >= 'A' && ch <= 'Z') {
-                camelCased.push(" ");
-                camelCased.push(ch);
-            } else {
-                camelCased.push(ch);
-            }
-        }
-        return camelCased;
-    }
-
-    return camel.join("");
+    return string.replace(/[A-Z]/g, val => ' ' + val);
 }
 
 console.log(solution("camelCase"));
