acbf9afe151b9f8fc8f8577908405eab6dbeea27
Paxnamra <bialyfeniks4@gmail.com>
2020-10-25 23:52:18 +0100

    code wars: mexican wave - solved kata

---
+++ mexican-wave/main.js
@@ -0,0 +1,31 @@
+/*
+result = ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
+Test.assertDeepEquals(wave("hello"),result, "Should return: '"+result+"'");
+
+result = ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"];
+Test.assertDeepEquals(wave("codewars"), result, "Should return: '"+result+"'");
+
+result = [];
+Test.assertDeepEquals(wave(""), result, "Should return: '"+result+"'");
+
+result = ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"];
+Test.assertDeepEquals(wave("two words"), result, "Should return: '"+result+"'");
+
+result = [" Gap ", " gAp ", " gaP "];
+Test.assertDeepEquals(wave(" gap "), result, "Should return: '"+result+"'");
+ */
+
+function wave(str) {
+    let resultArray = [];
+    let strArr = str.split('');
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/[a-z]/g)) {
+            strArr[i] = str.charAt(i).toUpperCase();
+            resultArray.push(strArr.join(''));
+            strArr[i] = str.charAt(i).toLowerCase();
+        }
+    }
+
+   return resultArray;
+}
